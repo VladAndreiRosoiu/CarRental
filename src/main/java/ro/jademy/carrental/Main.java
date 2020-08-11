e74d65e3c763100dae2ce9b3002df4d1b2bd4c52
Vlad Andrei Rosoiu <rosoiuandreivlad@gmail.com>
2020-08-11 19:02:11 +0300

    edited app files

--- src/main/java/ro/jademy/carrental/Main.java
+++ src/main/java/ro/jademy/carrental/Main.java
@@ -1,8 +1,23 @@
 package ro.jademy.carrental;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
-        // Q: this is the main entry point of our program. What should we do here?
+
+        ro.jademy.carrental.data.CarList carList=new ro.jademy.carrental.data.CarList();
+        ro.jademy.carrental.data.UserList userList=new ro.jademy.carrental.data.UserList();
+        RentalShop rentalShop =new RentalShop(userList.getUserList(), carList.getCarList());
+        rentalShop.rentalShop();
+
     }
+
+
+    //TODO:
+    // add admin logic - admin can add/remove cars
+    //                     - admin can add/remove users
+    //                     - admin can see rented cars and income produced
+    // add client logic - client can borrow car(add validation for driving licence issue date)
+    //                      - client will have to pay for rented car + add guarantee a deposit before renting car.
+    //                      - client can return car. After rent the deposit must be returned to client if car has not been damaged
+
 }
